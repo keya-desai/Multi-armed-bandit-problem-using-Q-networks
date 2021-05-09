2a93b677ea09e2f87f70093781c436626f67dd9b
prakruti-joshi <prakrutijoshi10@gmail.com>
2021-05-09 12:03:28 -0400

    Modified replay buffer

--- Code/DQN/q_network.py
+++ Code/DQN/q_network.py
@@ -133,29 +133,15 @@ class DQNModel(Solver):
             current_state = np.copy(next_state) 
 
     def replay_buffer(self, k = 2):
-        self.buffer_data = np.asarray(self.buffer_data)
-        print("Buffer data shape: ", self.buffer_data.shape)
-
         # Sampling k data points from replay buffer
-        num_rows = self.buffer_data.shape[0]
-        # num_rows = len(self.buffer_data)
+        num_rows = len(self.buffer_data)
+        # print("Num rows: ", num_rows)
         random_indices = np.random.choice(num_rows, size = k, replace=False)
         # print("random_indices = ", random_indices)
 
-        sampled_data = self.buffer_data[random_indices, :]
-        # print("buffer = ", self.buffer_data)
-        # print("sampled_data = ", sampled_data)
-
-        # sampled_data = list(sampled_data)
-        # Input output
-        input_data = sampled_data[:,:2]
-        print("Input data shape: ", input_data.shape)
-        output_data = sampled_data[:][-1]
-        print("Output data shape: ", output_data.shape)
-
-        # input_data, output_data = list()
-
-        self.Q_value_selected.fit( list(input_data),  list(output_data) , epochs = 1, verbose = False ) 
+        for idx in random_indices:
+          # self.Q_value_selected.fit(self.buffer_input_data[idx],  np.asarray([self.buffer_output_data[idx]), epochs = 1, verbose = False ) 
+          self.Q_value_selected.fit(self.buffer_data[idx][:2],  np.asarray([self.buffer_data[idx][2]]), epochs = 1, verbose = False ) 
         self.buffer_data = []
         return
 
